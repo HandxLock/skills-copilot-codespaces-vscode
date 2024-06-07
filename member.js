4502890d0b9994fa223b72b3d35f760251f6d61c
Erich Armijo <109211800+HandxLock@users.noreply.github.com>
2024-06-07 02:09:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,3 @@
+function skillsMember() {
+    return 'Node.js, React, React Native, JavaScript, HTML, CSS, SQL, MongoDB, Express.js, Git, GitHub, Heroku, Netlify, Agile, Scrum, Kanban, Trello, Slack, Figma, Adobe XD, Photoshop, Illustrator, After Effects';
+}
